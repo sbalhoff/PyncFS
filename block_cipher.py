8c288be57ca8aad317f51bebb5d999a7e165926c
Stephen Balhoff <sbalhoff@gmail.com>
2015-12-01 23:36:15 -0600

    Name changes, mostly. We're encrypting more than one block, etc...

--- block_cipher.py
+++ block_cipher.py
@@ -7,9 +7,9 @@ class BlockCipher():
     group_block_multiple = 10
     block_size = 16
 
-    metadata_header_length = 80
     digest_size = 64
     iv_size = 16
+    metadata_header_length = digest_size + iv_size
 
     def __init__(self, enc_key, sign_key):
         self.group_block_size = self.block_size * self.group_block_multiple
@@ -18,12 +18,12 @@ class BlockCipher():
         self.signing_key = sign_key
 
     # Returns tuple of (encrypted_data, metadata)
-    def encrypt_block(self, data):
+    def encrypt_data(self, data):
         enc_data = encrypt(data, self.encryption_key, self.signing_key)
-        meta = self.get_block_metadata(data, enc_data)
+        meta = self.get_metadata(data, enc_data)
         return (enc_data, meta)
 
-    def get_block_metadata(self, plain_data, enc_data):
+    def get_metadata(self, plain_data, enc_data):
         padlength = padding_length(len(plain_data))
         digest = enc_data[0:self.digest_size]
         iv = enc_data[self.digest_size:self.digest_size+self.iv_size]
@@ -35,7 +35,7 @@ class BlockCipher():
         }
         return m_data
 
-    def decrypt_with_metadata(self, data, metadata):
+    def decrypt_data(self, data, metadata):
         data = metadata['digest'] + metadata['iv'] + data + metadata['padding']
         return decrypt(data, self.encryption_key, self.signing_key)
 
@@ -49,7 +49,7 @@ class BlockCipher():
 
         data = os.read(fh, readlength)
         if len(data) > 0:
-            data = self.decrypt_with_metadata(data, metadata)
+            data = self.decrypt_data(data, metadata)
 
         return data[offset:(offset + length)]
 
@@ -64,14 +64,14 @@ class BlockCipher():
                 if len(data) > 0:
                     # Skipped data is 0 so don't decrypt
                     if not is_all_zero(data) and offset != 0:
-                        data = self.decrypt_with_metadata(data, metadata)
+                        data = self.decrypt_data(data, metadata)
                     plaintext = data[:offset] + buf + data[(offset + len(buf)):]
 
         except IOError:
             plaintext = buf
         
         #encrypt and write the metadata file
-        enc_block = self.encrypt_block(plaintext)
+        enc_block = self.encrypt_data(plaintext)
         enc_data = enc_block[0]
         metadata = enc_block[1]
 
